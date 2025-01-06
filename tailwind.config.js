958f0e5bbcaefc2dd8c02aea68669c545c2fb1f6
jcruzbravo2000 <jcruzbravo2000@gmail.com>
2025-01-05 21:15:08 -0500

    remove unnecessary plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,6 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/flowbite/**/*.js",
   ],
   theme: {
     extend: {
@@ -12,8 +11,6 @@ export default {
       }
     },
   },
-  plugins: [
-      require('flowbite/plugin')
-  ],
+  plugins: [],
 }
 
